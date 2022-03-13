baff040adb1ac1ed255ff88e461f41f57d941bbc
AEV0129 <AEV0129@daugherty.com>
2022-03-13 17:49:06 -0500

    corrected the way to reference var/run in windows. you need a double '/' as in '//'

--- .scripts/start-ado-linux-agent.sh
+++ .scripts/start-ado-linux-agent.sh
@@ -15,7 +15,7 @@ then
     docker pull cywl/azureagent
     # Run cywl/azure-agent docker image
     docker run -d \
-      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v //var/run/docker.sock:/var/run/docker.sock \
       -e "AZP_URL=https://dev.azure.com/anibalcincovelarde" \
       -e "AZP_TOKEN=$my_pat_token" \
       -e "AZP_AGENT_NAME=homebrewedagent" \
